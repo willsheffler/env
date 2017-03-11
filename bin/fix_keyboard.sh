407b8b5989a0d9641866cd5fbd8f8d17aac49e7d
Will Sheffler <willsheffler@gmail.com>
2017-03-11 05:29:00 -0800

    move kbd stuff to env

--- bin/fix_keyboard.sh
+++ bin/fix_keyboard.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 setxkbmap -option ctrl:nocaps
-xkbcomp $HOME/env/logitech_k400_RALT_to_LWIN.xkb $DISPLAY
+if [[ $HOSTNAME == "vivid" ]]; then
+    echo "================ on vivid ========================="
+    xkbcomp "$HOME/env/xkb/logitech_k400_RALT_to_LWIN.xkb" "$DISPLAY"
+elif [[ $HOSTNAME == "lappy" ]]; then
+    echo "================ on lappy ========================="
+    xkbcomp "$HOME/env/xkb/lappy_logitech_k400.xkb" "$DISPLAY"
+    echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
+    # k400ids=$(xinput list | sed -n "s/.*Logitech\ K400.*id=\([0-9]*\).*pointer.*/\1/p")
+    # for k400id in $k400ids; do
+        # echo "k400 k400id: $k400id"
+        # xkbcomp -i "$k400id" "$HOME/env/xkb/lappy_logitech_k400.xkb" "$DISPLAY"
+
+    # done
+    # internal_ids=$(xinput list | sed -n "s/.*Apple\ Internal\ Keyboard.*id=\([0-9]*\).*keyboard.*/\1/p")
+    # for internal_id in $internal_ids; do
+        # echo "k400 internal_id: $internal_id"
+        # xkbcomp -i "$internal_id" "$HOME/env/xkb/lappy_internal_orig.xkb" "$DISPLAY"
+    # done
+else
+    echo "don't know about hostname $HOSTNAME"
+fi
