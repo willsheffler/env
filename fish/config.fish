3b582377ee17695163e023ae33437406f5b074a2
Will Sheffler <willsheffler@gmail.com>
2024-07-16 02:17:51 -0700

    add kitty qtile

--- fish/config.fish
+++ fish/config.fish
@@ -5,14 +5,18 @@ eval /home/sheffler/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 conda activate rfd
 # source $HOME/venv/sci/bin/activate.fish
 
-set -x __NV_PRIME_RENDER_OFFLOAD 1
-set -x __GLX_VENDOR_LIBRARY_NAME nvidia
+set -gx EDITOR 'subl -w'
 
-set -x OMP_NUM_THREADS 4
-set -x MKL_NUM_THREADS 4
+set -gx __NV_PRIME_RENDER_OFFLOAD 1
+set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
 
-#set -x LD_PRELOAD '/home/sheffler/tools/graphbolt/lib/mimalloc/out/release/libmimalloc.so'
-set -x APPTAINER_CONTAINER '/software/containers/users/sheffler/rf_diffusion_aa_py310.sif'
-set -x PY /home/sheffler/venv/sci/bin/python
+set -gx OMP_NUM_THREADS 4
+set -gx MKL_NUM_THREADS 4
+
+#set -gx LD_PRELOAD '/home/sheffler/tools/graphbolt/lib/mimalloc/out/release/libmimalloc.so'
+set -gx APPTAINER_CONTAINER '/software/containers/users/sheffler/rf_diffusion_aa_py310.sif'
+set -gx PY /home/sheffler/venv/sci/bin/python
 # function fish_prompt; echo "-> "; end
 # function fish_prompt_right; echo ""; end
+
+direnv hook fish | source
---
+++ qtile/autostart.sh
@@ -0,0 +1,6 @@
+blueman-applet &
+nm-applet &
+
+setxkbmap us -variant colemak
+setxkbmap -option ctrl:nocaps
+xrandr --output DP-2 --mode 3840x2160 --rate 120.01
